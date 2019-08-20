915e3c0ad5b5f67bd21159d6f83780ee5c092041
Sword-Win7-32 <283901189@qq.com>
2019-08-20 17:31:36 +0800

    Grades.py first add

---
+++ Grades.py
@@ -0,0 +1,37 @@
+from MITPerson import MITPerson
+
+class Grade(objcet):
+    def __init__(self):
+        """ 创建一个空的成绩册 """
+        self.students = []
+        self.grades = {}
+        self.isSorted = True
+
+    def addStudent(self, student):
+        """ 假设student为Student类型，将student添加到成绩册 """
+        if student in self.students:
+            raise ValueError('Duplicate student')
+        self.students.append(student)
+        self.grades[student.getIdNum()] = []
+        self.isSorted = False
+
+    def addGrade(self, student, grade):
+        """ 假设grade为浮点数，将grade添加到student的成绩列表 """
+        try:
+            self.grades[student.getIdNum()].append(grade)
+        except:
+            raise ValueError('Student not in mapping')
+
+    def getGrades(self, student):
+        """ 返回student的成绩列表 """
+        try:
+            return self.grades[student.getIdNum()][:]
+        except:
+            raise ValueError('Student not in mapping')
+
+    def getStudents(self):
+        """ 返回成绩册中排号序的成绩列表 """
+        if not self.isSorted:
+            self.students.sort()
+            self.isSorted = True
+        return self.students[:]
